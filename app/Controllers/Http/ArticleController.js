e7cb9a0269d1bf9bb269da5938f13b151316ad3e
AnnaFragoso <anna.barbara.fragoso@hotmail.com>
2019-07-09 18:06:19 -0300

    Rota Article: post(funcionando)

--- app/Controllers/Http/ArticleController.js
+++ app/Controllers/Http/ArticleController.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const Article = use('App/Models/Article');
-const Auth = use('App/Models/Auth');
+//const Auth = use('App/Models/Auth');
 const User = use('App/Models/User');
 
 class ArticleController {
@@ -9,7 +9,7 @@ class ArticleController {
     async store({ request, response, auth }) {
         const data = request.only(["title", "body", "id_category"]);
 
-        data.id_user = auth.user.id;
+        data.id_users = auth.user.id;
 
         const post = await Article.create(data);
        // console.log(post);
